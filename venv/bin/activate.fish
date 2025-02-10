3a900061c7c4e10c399b7f3575ec79e88dfd0c50
Quan Duong <122609638+Quanthenewbiecoder@users.noreply.github.com>
2025-02-10 15:28:56 +0000

    Venv environment update

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/workspaces/122609638/finalproject/personal-finance-manager/venv"
+set -gx VIRTUAL_ENV "/workspaces/Personal-finance-manager/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
